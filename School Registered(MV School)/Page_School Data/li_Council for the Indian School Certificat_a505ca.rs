dd5d41f5f28de88beba6ec9da0b30a6bb3a3a8bf
bhaguSawant <142791525+bhaguSawant@users.noreply.github.com>
2023-10-10 16:42:21 +0530

    Add files via upload

---
+++ School Registered(MV School)/Page_School Data/li_Council for the Indian School Certificat_a505ca.rs
@@ -0,0 +1,139 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>li_Council for the Indian School Certificat_a505ca</name>
+   <tag></tag>
+   <elementGuidId>bf7eb935-3cb2-410b-8ef6-ddc09a5bafdd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#PopupLov_6_P6_BOARD_dlg > div.a-PopupLOV-results.a-TMV > div.a-TMV-body > div.a-TMV-w-scroll > ul.a-IconList > li.a-IconList-item</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='PopupLov_6_P6_BOARD_dlg']/div[3]/div/div[3]/ul/li</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>li</value>
+      <webElementGuid>7d136b4c-59a5-4005-ac5d-d881c71d880b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-id</name>
+      <type>Main</type>
+      <value>3</value>
+      <webElementGuid>ccd73c28-0fc9-464d-b006-1647d003c8a6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>a-IconList-item</value>
+      <webElementGuid>a39e1bee-28aa-4164-94c9-0de92db7d7dc</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>option</value>
+      <webElementGuid>79c722bc-06a0-437b-b779-52f8b5018b01</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tabindex</name>
+      <type>Main</type>
+      <value>0</value>
+      <webElementGuid>36914da0-d992-4da0-9323-72daf78096fe</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Council for the Indian School Certificate Examination (CISCE)</value>
+      <webElementGuid>bbd289a4-2fe7-407c-8fd8-93bfc5f76ff1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;PopupLov_6_P6_BOARD_dlg&quot;)/div[@class=&quot;a-PopupLOV-results a-TMV&quot;]/div[@class=&quot;a-TMV-body&quot;]/div[@class=&quot;a-TMV-w-scroll&quot;]/ul[@class=&quot;a-IconList&quot;]/li[@class=&quot;a-IconList-item&quot;]</value>
+      <webElementGuid>a0ce716f-f2e3-4a65-b25f-bc2c91106439</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='PopupLov_6_P6_BOARD_dlg']/div[3]/div/div[3]/ul/li</value>
+      <webElementGuid>c94936b0-2a2f-4bc5-9f70-5817f8d9e9f4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Select List 6 rows. Use arrow keys'])[2]/following::li[1]</value>
+      <webElementGuid>db94321b-2672-46e2-8ed4-2c3c39b01f23</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='No results found.'])[4]/following::li[1]</value>
+      <webElementGuid>c37f928b-ebac-4046-88d1-3f7c8d44c9f3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Cambridge International Examinations (CIE)'])[1]/preceding::li[1]</value>
+      <webElementGuid>dacd9c76-eb95-4481-92ef-fdf1bb54faf0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Central Board of Secondary Education (CBSE)'])[1]/preceding::li[2]</value>
+      <webElementGuid>4016c6bb-36f7-47eb-9c7f-1d19f0a68d61</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Council for the Indian School Certificate Examination (CISCE)']/parent::*</value>
+      <webElementGuid>1be716f4-2a59-466c-bd8a-aaae40f964a4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[8]/div[2]/div[3]/div/div[3]/ul/li</value>
+      <webElementGuid>a24d6af4-d098-48e0-b03a-e81b3830a7ad</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//li[(text() = 'Council for the Indian School Certificate Examination (CISCE)' or . = 'Council for the Indian School Certificate Examination (CISCE)')]</value>
+      <webElementGuid>077bf3fb-b233-4994-a99c-7565ac938731</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
